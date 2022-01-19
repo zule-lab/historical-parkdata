d6f1276e18d389ba8cdc137c5b6af8971645136c
icrichmond <icrichmond@mun.ca>
2022-01-19 14:55:18 -0500

    fix loop

--- script/0-TestRun.R
+++ script/0-TestRun.R
@@ -1,4 +1,4 @@
-install.packages('tesseract', 'magick', 'pdftools')
+#install.packages('tesseract', 'magick', 'pdftools')
 p <- c("tesseract", "magick", "pdftools")
 lapply(p, library, character.only = T)
 #tesseract_download("fra") # only necessary once
@@ -8,16 +8,17 @@ eng <- tesseract("eng")
 fr <- tesseract("fra")
 
 # convert pdf to high res png 
-pngfile <- pdf_convert("C:/Users/I_RICHMO/Downloads/VM166-D01901-56_op parc Louis-Cyr.pdf", dpi = 600)
+pngfile <- pdf_convert("pdf/Parc/VM166-D01901-56_op parc Louis-Cyr.pdf", pages = 5:10, dpi = 600)
 
 # preprocess png for optimal conditions 
-text <- input %>%
-  image_resize("2000x") %>%
-  image_convert(type = 'Grayscale') %>%
-  image_trim(fuzz = 40) %>%
+text <- image_read(pngfile) %>%
+  image_modulate(brightness = 80, saturation = 120, hue = 90) %>%
+  image_contrast(sharpen = 5) %>%
+  image_enhance() %>%
   image_write(format = 'png', density = '300x300') %>%
   tesseract::ocr() 
 
 # use optimal character recognition 
 text <- ocr(pngfile)
 cat(text)
+
